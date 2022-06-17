f86d5d7f09f7afb686841c7fe9ab06b9d6b86dd1
Anthony Guerrero <adguerrero1935@gmail.com>
2022-06-17 17:46:50 -0400

    Set up the express/node connection for the server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,21 @@
-import { Express } from "express";
+import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import cors from 'cors';
+
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+
+const CONNECTION_URL = "mongodb+srv://adguerrero1989:<PASSWORD>@cluster0.zujsw.mongodb.net/?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log('sever is running on port:', PORT)))
+    .catch((err) => console.error(err.message));
+
+// mongoose.set('useFindAndModify', false);
